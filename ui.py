d23e914072e95b8bcd38e8e9f68ec7d4b44bf242
mnguy <mnguy@FUNNGU2505>
2025-08-19 15:14:02 +0700

    Hoàn thành 95% tính năng hookingInitial commit

---
+++ ui.py
@@ -0,0 +1,36 @@
+﻿from rich.console import Console
+from rich.table import Table
+from rich.text import Text
+from rich.padding import Padding
+from pandas import DataFrame
+
+def print_user_table_clean(data:DataFrame)->None:
+    console = Console()
+    table = Table(
+        show_header=True,
+        header_style="bold white on black",
+        border_style="white", 
+        padding=(0, 0)
+    )
+
+    table.add_column("STT", justify="center")
+    for col in data.columns:
+        table.add_column(str(col), justify="center")
+
+    # Hàng trắng đầu tiên
+    top_blank = [Text("") for _ in range(len(data.columns) + 1)]
+    table.add_row(*top_blank)
+
+    for i, row in data.iterrows():
+        styled_row = [Text(str(i), style="black on white")]
+        for cell in row:
+            value = str(cell) if str(cell) != "nan" else " "
+            styled_row.append(Text(value, style="black on white"))
+        table.add_row(*styled_row)
+
+        if i < len(data) - 1:
+            table.add_row(*top_blank)
+    padded_table = Padding(table, (0, 2)) 
+    console.print(padded_table)
+
+
